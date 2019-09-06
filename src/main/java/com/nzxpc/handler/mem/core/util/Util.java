7c326d691efbe413b79a50f808a19a1bc4768cdf
梁涛 <320603976@qq.com>
2019-09-06 12:06:13 +0800

    修改

--- src/main/java/com/nzxpc/handler/mem/core/util/Util.java
+++ src/main/java/com/nzxpc/handler/mem/core/util/Util.java
@@ -1,9 +1,26 @@
 package com.nzxpc.handler.mem.core.util;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.UUID;
 
 public class Util {
     public static String uuid() {
         return UUID.randomUUID().toString().replace("-", "");
     }
+
+    /**
+     * 以百分比形式输出，输出形式：0.2345=>23.54%，0.1=>10.00%,
+     * 若超过了4位小数，则四舍五入
+     */
+    public static String rate(BigDecimal bd) {
+        if (bd == null || bd.signum() == 0) {
+            return "0.00%";
+        }
+        DecimalFormat decimalFormat = new DecimalFormat("0.00%");
+        return decimalFormat.format(bd);
+    }
+    /**
+     * 最大精度 0.2233555=> 22.33% 超过四舍五入 精度四一下都是两位,往上都是四舍五入取精度
+     */
 }
