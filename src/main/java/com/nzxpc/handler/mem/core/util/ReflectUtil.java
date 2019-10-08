ddca4a4e218b4ba3995aaf8ef439b90e5a2a13fb
梁涛 <320603976@qq.com>
2019-10-08 10:15:29 +0800

    新增

--- src/main/java/com/nzxpc/handler/mem/core/util/ReflectUtil.java
+++ src/main/java/com/nzxpc/handler/mem/core/util/ReflectUtil.java
@@ -1,10 +1,16 @@
 package com.nzxpc.handler.mem.core.util;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.core.type.filter.AssignableTypeFilter;
 
-import java.util.Set;
+import javax.persistence.EmbeddedId;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class ReflectUtil {
@@ -29,7 +35,71 @@ public class ReflectUtil {
         }
 
     }
+
     /**
      * lanmda表达式获取字段，调用代码块中的逻辑进行处理 放在lan代码块中处理
      */
+    public static void reflectAllField(Class<?> clazz, Consumer<Field> fieldConsumer) {
+        for (; clazz != Object.class; clazz = clazz.getSuperclass()) {
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                fieldConsumer.accept(field);
+            }
+        }
+    }
+
+    /**
+     * 反射获取对象属性值，包括所有上级类，其中枚举取ordinal值
+     */
+    public static Map<String, Object> getAllFields(Object bean, List<String> targetPropList) {
+        Map<String, Object> ret = new HashMap<>();
+        reflectAllField(bean.getClass(), field -> {
+            if (ret.containsKey(field.getName())) {
+                return;//继承如果存在同名的属性，则通过反射从父类获取的是null
+            }
+            boolean isStatic = Modifier.isStatic(field.getModifiers());
+            if (isStatic) {
+                return;//忽略静态属性
+            }
+            boolean isOk = targetPropList == null;
+            Optional<String> item = Optional.empty();
+            if (!isOk) {
+                item = targetPropList.stream().filter(a -> StringUtils.equalsIgnoreCase(a, field.getName())).findFirst();
+                isOk = item.isPresent();
+            }
+            if (isOk) {
+                field.setAccessible(true);
+                Object val = null;
+                try {
+                    val = field.get(bean);
+
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+                if (val == null) {
+                    if (field.getType() == BigDecimal.class) {
+                        val = BigDecimal.ZERO;
+                    } else {
+                        return;
+                    }
+                }
+                if (field.isAnnotationPresent(EmbeddedId.class)) {
+                    Map<String, Object> map = getAllFields(val, targetPropList);
+                    ret.putAll(map);
+                    return;
+                }
+                if (val instanceof Enum<?>) {
+                    Enum<?> val1 = (Enum<?>) val;
+                    val = val1.ordinal();
+                }
+                ret.put((item != null && item.isPresent()) ? item.get() : field.getName(), val);
+            }
+
+        });
+        return ret;
+    }
+
+    public static Map<String, Object> getAllFields(Object bean) {
+        return getAllFields(bean, null);
+    }
 }
--- src/main/java/com/nzxpc/handler/mem/core/util/StrUtil.java
+++ src/main/java/com/nzxpc/handler/mem/core/util/StrUtil.java
@@ -1,6 +1,26 @@
 package com.nzxpc.handler.mem.core.util;
 
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.StringJoiner;
+
 public class StrUtil {
     private static final String SEP1 = ",";
     private static final String SEP2 = "=";
+
+    private static String obj2Str(Object obj) {
+        StringJoiner stringJoiner = new StringJoiner(SEP1, "{", "}");
+        Map<String, Object> map = ReflectUtil.getAllFields(obj);
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            Object value = entry.getValue();
+            String valStr = "";
+            if (value instanceof BigDecimal) {
+                valStr = String.valueOf(((BigDecimal) value).doubleValue());
+            } else {
+                valStr = value.toString();
+            }
+            stringJoiner.add(entry.getKey() + SEP2 + valStr);
+        }
+        return stringJoiner.toString();
+    }
 }
