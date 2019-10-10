4b72ef910af173f313970598e7deada301f08f57
梁涛 <320603976@qq.com>
2019-10-10 14:39:33 +0800

    xz

--- src/main/java/com/nzxpc/handler/mem/core/ICache.java
+++ src/main/java/com/nzxpc/handler/mem/core/ICache.java
@@ -1,7 +1,13 @@
 package com.nzxpc.handler.mem.core;
 
 public interface ICache {
+    /**
+     *重放前执行
+     */
     void beforeReplay();
 
+    /**
+     *事件重放后执行，重放后的初始化
+     */
     void afterReplay();
 }
