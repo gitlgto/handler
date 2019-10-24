a91ca10b5a9c1abf6b63dbda74219894d2b1894e
梁涛 <320603976@qq.com>
2019-10-24 16:32:10 +0800

    操作数据库

--- src/main/java/com/nzxpc/handler/mem/core/util/db/MySqlHelper.java
+++ src/main/java/com/nzxpc/handler/mem/core/util/db/MySqlHelper.java
@@ -4,10 +4,14 @@ import com.google.common.collect.ImmutableMap;
 import com.nzxpc.handler.mem.core.entity.Getter;
 import com.nzxpc.handler.mem.core.entity.IdEntityBasePure;
 import com.nzxpc.handler.mem.core.util.ReflectUtil;
+import com.nzxpc.handler.mem.core.util.Util;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
@@ -97,7 +101,7 @@ public class MySqlHelper<T> extends SqlHelper<T> {
             sjName.add(parseName(propName));
             sjVal.add(":" + propName);
         }
-        ret.sql = sjName.toString() + "VALUES" + sjVal.toString();
+        ret.sql = sjName.toString() + " VALUES " + sjVal.toString();
         ret.map = new MapSqlParameterSource(fieldMap);
         return ret;
     }
@@ -117,7 +121,7 @@ public class MySqlHelper<T> extends SqlHelper<T> {
             sjName.add(parseName(propName));
             sjVal.add(":" + propName);
         }
-        ret.sql = sjName.toString() + "VALUES" + sjVal.toString();
+        ret.sql = sjName.toString() + " VALUES " + sjVal.toString();
         ret.map = new MapSqlParameterSource(map);
         return ret;
     }
@@ -147,7 +151,7 @@ public class MySqlHelper<T> extends SqlHelper<T> {
         if (argMap == null) {
             throw new RuntimeException("argMap不能为空");
         }
-        Map<String, Object> simple = getSimple("SELECT COUNT(0) `cnt` FROM " + parseName(tableName) + "WHERE" + conditionSql, argMap);
+        Map<String, Object> simple = getSimple("SELECT COUNT(0) `cnt` FROM " + parseName(tableName) + " WHERE " + conditionSql, argMap);
         return (long) simple.get("cnt") > 0;
     }
 
@@ -173,82 +177,137 @@ public class MySqlHelper<T> extends SqlHelper<T> {
                 }
             }
         }
+        //此步操作的目的是把传入的实体中数据取出，将要更新的字段和数据对应，存到map中返回，并返回sql。以供使用
         PrepareSqlParamResult result = prepareSqlParamForUpdate(data, Arrays.asList(updateColumns));
         result.map.addValues(conditionMap);
-        String sql = "UPDATE" + parseName(tableName) + " " + result.sql + "WHERE" + conditionSql;
+        String sql = " UPDATE " + parseName(tableName) + " " + result.sql + " WHERE " + conditionSql;
         return getJt().update(sql, result.map);
     }
 
     @Override
     public <IdEntity extends IdEntityBasePure, V> int updateById(IdEntity data, Getter<IdEntity, V>... getters) {
-
-        return 0;
+        String[] arr = new String[getters.length];
+        for (int i = 0; i < getters.length; i++) {
+            Getter<IdEntity, V> getter = getters[i];
+            //获取传入的方法对应的字段名
+            String prop = Util.prop(getter);
+            arr[i] = prop;
+        }
+        //传入操作的实体，和要修改的字段，一般这种更新，都是先给要修改的实体中的字段赋值，
+        // 然后传入该实体和要修改的字段（列），其中带sql的，则sql中的字段不能和要修改字段重复（列）。
+        return updateById(data, arr);
     }
 
     @Override
     public int deleteById(int id) {
-        return 0;
+        return delete("id=:id", ImmutableMap.of("id", id));
     }
 
     @Override
     public int delete(String conditionSql, Map<String, Object> conditionArgMap) {
-        return 0;
+        conditionArgMap = changeEum(conditionArgMap);
+        String sql = "DELETE FROM " + parseName(tableName) + " WHERE " + conditionSql;
+        return getJt().update(sql, conditionArgMap);
     }
 
     @Override
     public T getById(int id) {
-        return null;
+        return get("SELECT * FROM " + parseName(tableName) + " WHERE `id`=:id", ImmutableMap.of("id", id));
     }
 
+    /**
+     * 为了更新而获取，会加上行级锁，需要开启事务
+     *
+     * @param id
+     * @return
+     */
     @Override
     public T getByIdForUpdate(int id) {
-        return null;
+        return get("SELECT * FROM " + parseName(tableName) + " WHERE `id`=:id FOR UPDATE", ImmutableMap.of("id", id));
     }
 
+    /**
+     * 查找第一条数据
+     *
+     * @return 第一条被添加的数据
+     */
     @Override
     public T get() {
-        return null;
+        return get("SELECT * FROM " + parseName(tableName) + " LIMIT 1", null);
     }
 
     @Override
     public T getForUpdate() {
-        return null;
+        return get("SELECT * FROM " + parseName(tableName) + " LIMIT 1 FOR UPDATE", null);
     }
 
     @Override
     public T getForUpdate(String conditionSql, Map<String, Object> argMap) {
-        return null;
+        if (argMap == null) {
+            throw new RuntimeException("argMap不能为空");
+        }
+        argMap = changeEum(argMap);
+        String sql = "SELECT * FROM " + parseName(tableName) + " WHERE " + conditionSql + " LIMIT 1 FOR UPDATE";
+        return get(sql, argMap);
     }
 
     @Override
     public T get(String conditionSql, Map<String, Object> argMap) {
-        return null;
+        List<T> list = this.list(conditionSql, argMap);
+        if (list == null || list.size() == 0) {
+            return null;
+        } else {
+            return list.get(0);
+        }
     }
 
     @Override
     public <IdEntity extends IdEntityBasePure> void add(IdEntity t) {
-
+        MySqlHelper.PrepareSqlParamResult ret = prepareSqlParamForAdd(t);
+        String sql = "INSERT INTO " + parseName(tableName) + " " + ret.getSql();
+        //获取插入数据后的id
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        getJt().update(sql, ret.getMap(), keyHolder);
+        if (keyHolder.getKey() != null) {
+            t.setId(keyHolder.getKey().intValue());
+        }
     }
 
     @Override
     public <Entity> int addBatch(Collection<Entity> entities) {
-        return 0;
+        if (entities != null && !entities.isEmpty()) {
+            PrepareSqlParamResult result = prepareSqlParamForBatchAdd(entities.iterator().next());
+            String sql = "INSERT INTO " + parseName(tableName) + " " + result.getSql();
+            //获取的是每个index下都对应的一个map。
+            SqlParameterSource[] sources = prepareSqlParams(entities);
+            int[] ints = getJt().batchUpdate(sql, sources);
+            return ints.length;
+        } else {
+            return 0;
+        }
     }
 
     @Override
     public List<T> list() {
-        return null;
+        return this.list("SELECT * FROM " + parseName(tableName), null);
     }
 
     @Override
     public List<T> list(String sql, Map<String, Object> argMap) {
-        return null;
+        argMap = changeEum(argMap);
+        //使用beanpropertyrowmapper将数据库查询结果转换成java类对象，获取list结果列表，数据库表字段和实体类自动对应 persistentclass代表要操作的实体
+        @SuppressWarnings({"unchecked", "rawtypes"}) List<T> list = getJt().query(sql, argMap, new BeanPropertyRowMapper(persistentClass));
+        return list == null ? new ArrayList<T>() : list;
     }
 
     @Override
     public <Entity> void add(Entity t) {
-
+        PrepareSqlParamResult result = prepareSqlParamForAdd(t);
+        String sql = "INSERT INTO " + parseName(tableName) + " " + result.getSql();
+        getJt().update(sql, result.getMap());
     }
+
+
 
     @Override
     public int getCount(String sql, Map<String, Object> argMap) {
